e609c28d0cfb4bcd8c400d4f4858122d3a3ab686
Alexander Litskevich <a.litskevich@chillgaming.net>
2024-06-08 13:14:49 +0400

    Updated bot

--- bot.py
+++ bot.py
@@ -1,16 +1,31 @@
-from telegram import Update, WebAppInfo, ReplyKeyboardMarkup, KeyboardButton
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
+from telegram import Update, WebAppInfo, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, InlineQueryResultGame
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext, CallbackQueryHandler, InlineQueryHandler
 
 
 API_KEY = "7362459748:AAFyu1aRujXdxzmkY41K1NX0zk3-0WxyzXg"
 APP_URL = "https://alexandrlv.github.io/TestTelegramWebApp/index.html"
-
+GAME_URL = "https://game.rariaden-hub.ru/?v=1.0.2"
+GAME_SHORT_NAME = "ZombieShooter"
 
 async def start(update: Update, context: CallbackContext) -> None:
     keyboard = [[KeyboardButton("Open Web App", web_app=WebAppInfo(url=APP_URL))]]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
     await update.message.reply_text('Откройте веб-приложение', reply_markup=reply_markup)
 
+async def game(update: Update, context: CallbackContext) -> None:
+    await update.get_bot().send_game(chat_id=update.message.chat_id, game_short_name=GAME_SHORT_NAME)
+    #keyboard = [[InlineKeyboardButton("Play Game", callback_data=GAME_SHORT_NAME)]]
+    #reply_markup = InlineKeyboardMarkup(keyboard)
+    #await update.message.reply_text('Нажмите кнопку ниже, чтобы сыграть в игру.', reply_markup=reply_markup)
+
+async def game_inline(update: Update, context: CallbackContext) -> None:
+    query_id = update.inline_query.id
+    results = [InlineQueryResultGame(id = 1, game_short_name=GAME_SHORT_NAME)]
+    await context.bot.answer_inline_query(inline_query_id=query_id, results=results)
+
+async def handle_message(update: Update, context: CallbackContext) -> None:
+    await update.message.reply_text(f'Умею реагировать на команды /start, /app и /game')
+        
 async def handle_web_app_data(update: Update, context: CallbackContext) -> None:
     if update.message.web_app_data:
         received_data = update.message.web_app_data.data
@@ -18,12 +33,27 @@ async def handle_web_app_data(update: Update, context: CallbackContext) -> None:
     else:
         await update.message.reply_text(f'Умею реагировать на команды /start и /app')
 
+async def play_game(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    if query.game_short_name == GAME_SHORT_NAME:
+        await context.bot.answer_callback_query(callback_query_id=query.id, url=GAME_URL)
+    else:
+        await context.bot.answer_callback_query(callback_query_id=query.id, text="EMPTY")
+
+    # await query.answer()
+    # reply_keyboard = InlineKeyboardMarkup([[InlineKeyboardButton("Play", url=GAME_URL)]])
+    # await update.get_bot().send_game(chat_id=query.message.chat_id, game_short_name=GAME_SHORT_NAME, reply_markup=reply_keyboard)
+
 def main():
     application = Application.builder().token(API_KEY).build()
     
     application.add_handler(CommandHandler('start', start))
     application.add_handler(CommandHandler('app', start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_web_app_data))
+    application.add_handler(CommandHandler('game', game))
+    application.add_handler(InlineQueryHandler(game_inline))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    application.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, handle_web_app_data))
+    application.add_handler(CallbackQueryHandler(play_game))
 
     application.run_polling()
 
